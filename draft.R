9a1cc47b3cc24c2defcea4cef8351b1e767466c7
ixtapis <no-reply@ilagos.com>
2022-05-15 20:42:48 -0500

    added quartiles

--- draft.R
+++ draft.R
@@ -117,3 +117,53 @@ stations_incomes %>%
   filter(!is.na(income_ave)) %>% 
   ggplot(aes(income_ave, has_e, fill = has_e)) + 
   geom_boxplot(varwidth = TRUE, alpha=1/3)
+
+iee_data %>% 
+  group_by(has_e) %>% 
+  ggplot(aes(cut_width(fam_med_income,40000), fill = has_e)) +
+  geom_bar()
+
+iee_data_t <- iee_data %>% 
+  mutate(fam_quant = cut(fam_med_income,quantile(fam_med_income, na.rm =T),include.lowest = T))
+
+fam_prop <- iee_data %>% 
+  filter(!is.na(fam_med_income)) %>% 
+  group_by(has_e,fam_quant) %>%  
+  summarise(freq = n()) %>% 
+  group_by(fam_quant) %>% 
+  mutate(prop = round(freq/sum(freq),2)) %>% 
+  ungroup()
+
+iee_data %>% 
+  filter(!is.na(fam_med_income)) %>% 
+  group_by(has_e) %>% 
+  ggplot(aes(fam_quant, fill = has_e)) +
+  geom_bar() +
+  geom_text(data = fam_prop, aes(label = prop, x = fam_quant, y = c(rep(5000,4),rep(1000,4)))) +
+  scale_x_discrete(labels = c("Q1","Q2","Q3","Q4"))
+
+iee_data %>% count(fam_quant)
+
+cut_clean <- function(df) {
+  df %>% 
+  mutate(quant = str_remove_all(quant, "\\(|\\]")) %>% 
+  separate(quant, into = c('q1', 'q2'), sep=",", convert = TRUE) %>% 
+  mutate(across(q1:q2, ~ scales::dollar(.x)), 
+         quant = glue::glue("({q1},{q2}]"), q1 = NULL, q2 = NULL)
+}
+
+df <- data.frame(income = rnorm(1000,77345,30569))
+df$quant <- cut(df$income, quantile(df$income),include.lowest = T)
+
+flevel <- levels(df$quant[1])
+
+x <- flevel[1]
+
+clean_interval <- function(x) {
+  oc <- c(str_sub(x,1,1),str_sub(x,-1,-1))
+  x <- str_sub(x,2,str_length(x)-1)
+  x <- as.double(str_split(x,",",simplify=T))
+  x <- scales::dollar(x)
+  paste0(oc[1],x[1],",",x[2],oc[2])
+}
+lapply(flevel,clean_interval) 
